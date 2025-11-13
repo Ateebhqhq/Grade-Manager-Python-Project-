cc34c49e15dfa655db4d1c36abe5e57b333aeecc
Ateebhqhq <ateeb13october77@gmail.com>
2025-11-13 23:35:46 +0100

    main structure of the program made

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+class StudentManager:
+    def add_student ( students , name ): 
+        
+    def add_grade ( students , name , grade ): ...
+    def average ( students , name ): ...
+    def save_to_file ( students , filename ): ...
+    def load_from_file ( filename ): ...
+
+
+if __name__ == "__main__":
+
+    print("Welcome to the Student Grade Manager.")
+    print("Please choose an option: ")
+    user_selection = input("1. Add student \n 2. Add grade \n 3. Show average for student \n 4. Show all students \n 5. Save to file \n 6. Load from file \n 7. Exit")
+        
