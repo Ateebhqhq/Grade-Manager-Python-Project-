d1aa109ca412cbd134c5d75355f1f1af4414c68a
Ateebhqhq <ateeb13october77@gmail.com>
2025-11-21 10:38:42 +0100

    Getting a hang around passing parameters, integrating classes, and making dictionary.

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import json
+import numpy
 
 class StudentManager:
     students_data = {}
-    def add_student ( students , name ): 
-        students_data[name] = []
+    def __init__(students):
+        students.students_data = {}
+
+    def add_student(students ,name): 
+        if name in students.students_data:
+            print(f"Student {name} already exists.")
+            return
+        
+        else:
+            students_data[name] = []
     def add_grade ( students , name , grade ): 
         students_data[name] = [grade]
-    def average ( students , name ): ...
-    def save_to_file ( students , filename ): ...
-    def load_from_file ( filename ): ...
+    #def average ( students , name ): 
+        
+    #def save_to_file ( students , filename ): 
+    #def load_from_file ( filename ): 
 
 
 if __name__ == "__main__":
 
     print("Welcome to the Student Grade Manager.")
+    manager = StudentManager()
     while True:
         print("Please choose an option: ")
         user_selection = input("1. Add student \n 2. Add grade \n 3. Show average for student \n 4. Show all students \n 5. Save to file \n 6. Load from file \n 7. Exit")
-        
         match user_selection:
             case "1":
                 name_input = input("Enter name: \n")
-                s1 = StudentManager(name_input)
-                add_student(s1, name_input)
+                manager.add_student(name_input)
+
             case "2":
                 name_input = input("Enter name: \n")
                 if name_input in students_data:
